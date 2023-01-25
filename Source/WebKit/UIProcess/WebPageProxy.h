315221279463713a7af975df88b18f1158c4efea
Miguel Gomez <magomez@igalia.com>
2023-01-25 15:50:59 +0100

    [Responsiveness] Add API to manually check WebProcess responsiveness

--- Source/WebKit/UIProcess/WebPageProxy.h
+++ Source/WebKit/UIProcess/WebPageProxy.h
@@ -1508,6 +1508,7 @@ public:
     void dispatchProcessDidTerminate(ProcessTerminationReason);
     void willChangeProcessIsResponsive();
     void didChangeProcessIsResponsive();
+    void isWebProcessResponsive(CompletionHandler<void (bool isWebProcessResponsive)>&& callback);
 
 #if PLATFORM(IOS_FAMILY)
     void processWillBecomeSuspended();
