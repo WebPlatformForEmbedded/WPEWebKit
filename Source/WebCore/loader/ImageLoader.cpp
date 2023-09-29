21811dd81e1a40c8d98b6a60f1329b4f146f5d67
Jakub Gocoł <93707234+jakub-gocol-red@users.noreply.github.com>
2023-09-29 10:39:56 +0200

    Implement list of available images matching logic (#321)
    Spec: https://html.spec.whatwg.org/multipage/images.html#updating-the-image-data:list-of-available-images
    
    This is restricted to lazy loading for now.

--- Source/WebCore/loader/ImageLoader.cpp
+++ Source/WebCore/loader/ImageLoader.cpp
@@ -42,6 +42,7 @@
 #include "InspectorInstrumentation.h"
 #include "JSDOMPromiseDeferred.h"
 #include "LazyLoadImageObserver.h"
+#include "MemoryCache.h"
 #include "Page.h"
 #include "RenderImage.h"
 #include "RenderSVGImage.h"
@@ -164,6 +165,22 @@ void ImageLoader::clearImageWithoutConsideringPendingLoadEvent()
         imageResource->resetAnimation();
 }
 
+// https://html.spec.whatwg.org/multipage/images.html#updating-the-image-data:list-of-available-images
+bool ImageLoader::canReuseFromListOfAvailableImages(const CachedResourceRequest& request, const String& crossOriginAttribute)
+{
+    CachedResource* resource = MemoryCache::singleton().resourceForRequest(request.resourceRequest(), document().page()->sessionID());
+    if (!resource || resource->stillNeedsLoad() || resource->isPreloaded())
+        return false;
+
+    if (!crossOriginAttribute.isNull() && !document().securityOrigin().isSameOriginAs(*resource->origin()))
+        return false;
+
+    if (resource->options().credentials == FetchOptions::Credentials::SameOrigin && (crossOriginAttribute.isNull() || equalLettersIgnoringASCIICase(crossOriginAttribute, "use-credentials"_s)))
+        return false;
+
+    return true;
+}
+
 void ImageLoader::updateFromElement(RelevantMutation relevantMutation)
 {
     // If we're not making renderers for the page, then don't load images. We don't want to slow
@@ -214,7 +231,7 @@ void ImageLoader::updateFromElement(RelevantMutation relevantMutation)
         } else {
             if (m_lazyImageLoadState == LazyImageLoadState::None && isImageElement) {
                 auto& imageElement = downcast<HTMLImageElement>(element());
-                if (imageElement.isLazyLoadable() && document.lazyImageLoadingEnabled()) {
+                if (imageElement.isLazyLoadable() && document.lazyImageLoadingEnabled() && !canReuseFromListOfAvailableImages(request, crossOriginAttribute)) {
                     m_lazyImageLoadState = LazyImageLoadState::Deferred;
                     request.setIgnoreForRequestCount(true);
                 }
