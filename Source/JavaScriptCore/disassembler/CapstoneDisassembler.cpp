5a67a8720435befa82282786c5f1a598da1c7604
Keith Miller <keith_miller@apple.com>
2025-01-29 07:36:45 -0800

    Revert 287649@main since it breaks tests on ARM64 https://bugs.webkit.org/show_bug.cgi?id=286679 rdar://143819751
    Unreviewed, revert.
    
    * JSTests/wasm/stress/tail-call-simple-i64.js: Removed.
    * Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h:
    (JSC::MacroAssemblerARMv7::transfer64): Deleted.
    * Source/JavaScriptCore/b3/B3LowerToAir.cpp:
    * Source/JavaScriptCore/b3/B3LowerToAir32_64.cpp:
    * Source/JavaScriptCore/b3/B3MoveConstants.cpp:
    * Source/JavaScriptCore/b3/air/AirCustom.h:
    (JSC::B3::Air::WasmBoundsCheckCustom::forEachArg):
    * Source/JavaScriptCore/disassembler/CapstoneDisassembler.cpp:
    * Source/JavaScriptCore/jit/Width.h:
    * Source/JavaScriptCore/runtime/JSCJSValue.cpp:
    (JSC::JSValue::dumpInContextAssumingStructure const):
    * Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp:
    (JSC::Wasm::OMGIRGenerator::emitIndirectCall):
    (JSC::Wasm::OMGIRGenerator::createCallPatchpoint):
    (JSC::Wasm::prepareForTailCallImpl):
    (JSC::Wasm::OMGIRGenerator::createTailCallPatchpoint):
    (JSC::Wasm::OMGIRGenerator::addCall):
    (JSC::Wasm::OMGIRGenerator::createCallConstrainedArgs): Deleted.
    * Source/JavaScriptCore/wasm/WasmOMGIRGenerator32_64.cpp:
    (JSC::Wasm::OMGIRGenerator::addArguments):
    (JSC::Wasm::OMGIRGenerator::emitIndirectCall):
    (JSC::Wasm::OMGIRGenerator::createCallPatchpoint):
    (JSC::Wasm::prepareForTailCallImpl):
    (JSC::Wasm::OMGIRGenerator::createTailCallPatchpoint):
    (JSC::Wasm::OMGIRGenerator::addCall):
    (JSC::Wasm::OMGIRGenerator::createCallConstrainedArgs): Deleted.
    
    Canonical link: https://commits.webkit.org/289498@main
    
    (cherry picked from commit 82b8457380cf38489bd334ea7f5a8486cc9e217d)

--- Source/JavaScriptCore/disassembler/CapstoneDisassembler.cpp
+++ Source/JavaScriptCore/disassembler/CapstoneDisassembler.cpp
@@ -26,7 +26,6 @@
 #include "config.h"
 
 #if ENABLE(DISASSEMBLER) && USE(CAPSTONE)
-WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN
 
 #include "AssemblyComments.h"
 #include "Disassembler.h"
@@ -81,5 +80,4 @@ bool tryToDisassemble(const CodePtr<DisassemblyPtrTag>& codePtr, size_t size, vo
 
 } // namespace JSC
 
-WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
 #endif // ENABLE(DISASSEMBLER) && USE(CAPSTONE)
