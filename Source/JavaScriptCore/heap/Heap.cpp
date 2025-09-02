8fdd69fb202272c8e81d0f5621f94331213c35da
Dan Hecht <dan.hecht@apple.com>
2025-09-02 14:05:54 -0700

    [JSC] Avoid dangling pointers in WeakBlock list https://bugs.webkit.org/show_bug.cgi?id=298236 rdar://157587352
    Reviewed by Keith Miller.
    
    Before this change, DoublyLinkedList leaves the next/prev pointers
    in a dangling state when removing a node from the list. Then,
    if the node is re-added, the next/prev pointers are reset when necessary.
    
    Let's make a stronger invariant: if the node is not in the list,
    then the prev/next pointers are nullptr. (Note that the converse
    is not true for single element lists.)
    
    Then, add some asserts to verify the WeakSet's WeakBlock list lifecycle
    to try to help track down a mysterious crash. The WeakBlock ownership
    can be transferred from the WeakSet to the Heap, at which point
    the prev/next should be nulled out and, after this change, no longer
    dangling.
    
    Bonus: remove MarkedSpace::freeOrShrinkBlock() since it's never called.
    Canonical link: https://commits.webkit.org/299454@main
    
    (cherry picked from commit c90e64eec92dad9dfa49801778997c6e449ce1c6)

--- Source/JavaScriptCore/heap/Heap.cpp
+++ Source/JavaScriptCore/heap/Heap.cpp
@@ -2650,6 +2650,7 @@ bool Heap::shouldDoFullCollection()
 
 void Heap::addLogicallyEmptyWeakBlock(WeakBlock* block)
 {
+    RELEASE_ASSERT(!block->next() && !block->prev());
     m_logicallyEmptyWeakBlocks.append(block);
 }
 
@@ -2668,6 +2669,7 @@ bool Heap::sweepNextLogicallyEmptyWeakBlock()
         return false;
 
     WeakBlock* block = m_logicallyEmptyWeakBlocks[m_indexOfNextLogicallyEmptyWeakBlockToSweep];
+    RELEASE_ASSERT(!block->next() && !block->prev());
 
     block->sweep();
     if (block->isEmpty()) {
--- Source/WTF/wtf/DoublyLinkedList.h
+++ Source/WTF/wtf/DoublyLinkedList.h
@@ -41,8 +41,8 @@ public:
 
 template<typename T> inline DoublyLinkedListNode<T>::DoublyLinkedListNode()
 {
-    setPrev(0);
-    setNext(0);
+    setPrev(nullptr);
+    setNext(nullptr);
 }
 
 template<typename T> inline void DoublyLinkedListNode<T>::setPrev(T* prev)
@@ -90,8 +90,8 @@ private:
 };
 
 template<typename T> inline DoublyLinkedList<T>::DoublyLinkedList()
-    : m_head(0)
-    , m_tail(0)
+    : m_head(nullptr)
+    , m_tail(nullptr)
 {
 }
 
@@ -126,37 +126,33 @@ template<typename T> inline T* DoublyLinkedList<T>::tail() const
 
 template<typename T> inline void DoublyLinkedList<T>::push(T* node)
 {
+    ASSERT(!node->prev() && !node->next());
     if (!m_head) {
         ASSERT(!m_tail);
         m_head = node;
         m_tail = node;
-        node->setPrev(0);
-        node->setNext(0);
         return;
     }
 
     ASSERT(m_tail);
     m_head->setPrev(node);
     node->setNext(m_head);
-    node->setPrev(0);
     m_head = node;
 }
 
 template<typename T> inline void DoublyLinkedList<T>::append(T* node)
 {
+    ASSERT(!node->prev() && !node->next());
     if (!m_tail) {
         ASSERT(!m_head);
         m_head = node;
         m_tail = node;
-        node->setPrev(0);
-        node->setNext(0);
         return;
     }
 
     ASSERT(m_head);
     m_tail->setNext(node);
     node->setPrev(m_tail);
-    node->setNext(0);
     m_tail = node;
 }
 
@@ -177,6 +173,8 @@ template<typename T> inline void DoublyLinkedList<T>::remove(T* node)
         ASSERT(node == m_tail);
         m_tail = node->prev();
     }
+    node->setNext(nullptr);
+    node->setPrev(nullptr);
 }
 
 template<typename T> inline T* DoublyLinkedList<T>::removeHead()
