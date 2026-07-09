yoavweiss/WebKit

[ARMv7] OMG: clarify where the inputs into try_table catch handler blocks come from https://bugs.webkit.org/show_bug.cgi?id=298536
Reviewed by Yusuke Suzuki.

Bring in https://commits.webkit.org/297341@main.

Canonical link: https://commits.webkit.org/300220@main

Source/JavaScriptCore/wasm/WasmOMGIRGenerator32_64.cpp
@@ -883,7 +883,7 @@ private:
     Value* loadFromScratchBuffer(OSRBufferMode, unsigned& indexInBuffer, Value* pointer, B3::Type);
     void connectControlAtEntrypoint(OSRBufferMode, unsigned& indexInBuffer, Value* pointer, ControlData&, Stack& expressionStack, ControlData& currentData, bool fillLoopPhis = false);
     Value* emitCatchImpl(CatchKind, ControlType&, unsigned exceptionIndex = 0);
-    void emitCatchTableImpl(ControlData& entryData, const ControlData::TryTableTarget&, const Stack&);
+    void emitCatchTableImpl(ControlData& entryData, const ControlData::TryTableTarget&);
     RefPtr<PatchpointExceptionHandle> preparePatchpointForExceptions(BasicBlock*, PatchpointValue*);
 
     Origin origin();
@@ -4709,7 +4709,7 @@ Value* OMGIRGenerator::emitCatchImpl(CatchKind kind, ControlType& data, unsigned
     return buffer;
 }
 
-auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::TryTableTarget& target, const Stack& stack) -> void
+auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::TryTableTarget& target) -> void
 {
     auto block = m_proc.addBlock();
     m_rootBlocks.append({ block, usesSIMD() });
@@ -4763,8 +4763,8 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
         connectControlAtEntrypoint(SplitI64, indexInBuffer, pointer, topControlData, topExpressionStack, data);
     }
 
-    auto newStack = stack;
 
+    Stack resultStack;
     if (target.type == CatchKind::Catch || target.type == CatchKind::CatchRef) {
         unsigned offset = 0;
         for (unsigned i = 0; i < signature->template as<FunctionSignature>()->argumentCount(); ++i) {
@@ -4772,7 +4772,7 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
             Variable* var = m_proc.addVariable(toB3Type(type));
             Value* value = m_currentBlock->appendNew<MemoryValue>(m_proc, Load, toB3Type(type), origin(), buffer, offset * sizeof(uint64_t));
             set(var, value);
-            newStack.constructAndAppend(type, var);
+            resultStack.constructAndAppend(type, var);
             offset += type.kind == TypeKind::V128 ? 2 : 1;
         }
     }
@@ -4784,11 +4784,11 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
             m_currentBlock->appendNew<Const32Value>(m_proc, Origin(), JSValue::CellTag));
         set(var, exceptionWithTag);
         push(exceptionWithTag);
-        newStack.constructAndAppend(Type { TypeKind::RefNull, static_cast<TypeIndex>(TypeKind::Exnref) }, var);
+        resultStack.constructAndAppend(Type { TypeKind::RefNull, static_cast<TypeIndex>(TypeKind::Exnref) }, var);
     }
 
     auto& targetControl = m_parser->resolveControlRef(target.target).controlData;
-    unifyValuesWithBlock(newStack, targetControl);
+    unifyValuesWithBlock(resultStack, targetControl);
 
     m_currentBlock->appendNewControlValue(m_proc, Jump, origin(), FrequentedBlock(targetControl.targetBlockForBranch(), FrequencyClass::Normal));
     targetControl.targetBlockForBranch()->addPredecessor(block);
@@ -5072,7 +5072,7 @@ auto OMGIRGenerator::addEndToUnreachable(ControlEntry& entry, const Stack& expre
         data.endTryTable(advanceCallSiteIndex());
         auto targets = data.m_tryTableTargets;
         for (auto& target : targets)
-            emitCatchTableImpl(data, target, expressionStack);
+            emitCatchTableImpl(data, target);
         --m_tryCatchDepth;
     }
 

WebPlatformForEmbedded/WPEWebKit

(cherry picked from commit b627e177be18f30eda11738c43f6fa97ccfebdcb)

Source/JavaScriptCore/wasm/WasmOMGIRGenerator32_64.cpp
@@ -1024,7 +1024,7 @@ private:
     Value* loadFromScratchBuffer(OSRBufferMode, unsigned& indexInBuffer, Value* pointer, B3::Type);
     void connectControlAtEntrypoint(OSRBufferMode, unsigned& indexInBuffer, Value* pointer, ControlData&, Stack& expressionStack, ControlData& currentData, bool fillLoopPhis = false);
     Value* emitCatchImpl(CatchKind, ControlType&, unsigned exceptionIndex = 0);
-    void emitCatchTableImpl(ControlData& entryData, const ControlData::TryTableTarget&, const Stack&);
+    void emitCatchTableImpl(ControlData& entryData, const ControlData::TryTableTarget&);
     PatchpointExceptionHandle preparePatchpointForExceptions(BasicBlock*, PatchpointValue*);
 
     Origin origin();
@@ -4732,7 +4732,7 @@ Value* OMGIRGenerator::emitCatchImpl(CatchKind kind, ControlType& data, unsigned
     return buffer;
 }
 
-auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::TryTableTarget& target, const Stack& stack) -> void
+auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::TryTableTarget& target) -> void
 {
     auto block = m_proc.addBlock();
     m_rootBlocks.append(block);
@@ -4786,8 +4786,8 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
         connectControlAtEntrypoint(SplitI64, indexInBuffer, pointer, topControlData, topExpressionStack, data);
     }
 
-    auto newStack = stack;
 
+    Stack resultStack;
     if (target.type == CatchKind::Catch || target.type == CatchKind::CatchRef) {
         unsigned offset = 0;
         for (unsigned i = 0; i < signature->template as<FunctionSignature>()->argumentCount(); ++i) {
@@ -4795,7 +4795,7 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
             Variable* var = m_proc.addVariable(toB3Type(type));
             Value* value = append<MemoryValue>(heapTop(), m_proc, Load, toB3Type(type), origin(), buffer, offset * sizeof(uint64_t));
             set(var, value);
-            newStack.constructAndAppend(type, var);
+            resultStack.constructAndAppend(type, var);
             offset += type.kind == TypeKind::V128 ? 2 : 1;
         }
     }
@@ -4807,11 +4807,11 @@ auto OMGIRGenerator::emitCatchTableImpl(ControlData& data, const ControlData::Tr
             m_currentBlock->appendNew<Const32Value>(m_proc, Origin(), JSValue::CellTag));
         set(var, exceptionWithTag);
         push(exceptionWithTag);
-        newStack.constructAndAppend(Type { TypeKind::RefNull, static_cast<TypeIndex>(TypeKind::Exn) }, var);
+        resultStack.constructAndAppend(Type { TypeKind::RefNull, static_cast<TypeIndex>(TypeKind::Exn) }, var);
     }
 
     auto& targetControl = m_parser->resolveControlRef(target.target).controlData;
-    unifyValuesWithBlock(newStack, targetControl);
+    unifyValuesWithBlock(resultStack, targetControl);
 
     m_currentBlock->appendNewControlValue(m_proc, Jump, origin(), FrequentedBlock(targetControl.targetBlockForBranch(), FrequencyClass::Normal));
     targetControl.targetBlockForBranch()->addPredecessor(block);
@@ -5101,7 +5101,7 @@ auto OMGIRGenerator::addEndToUnreachable(ControlEntry& entry, const Stack& expre
         data.endTryTable(advanceCallSiteIndex());
         auto targets = data.m_tryTableTargets;
         for (auto& target : targets)
-            emitCatchTableImpl(data, target, expressionStack);
+            emitCatchTableImpl(data, target);
         --m_tryCatchDepth;
     }
 